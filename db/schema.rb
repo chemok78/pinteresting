b26f5c4fb6969d282468d8a8ba195d506e4097bc
chemok78 <chemok78@gmail.com>
2014-09-18 14:55:22 +0800

    fix db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140915090222) do
+ActiveRecord::Schema.define(version: 20140917155248) do
+
+  create_table "pins", force: true do |t|
+    t.string   "description"
+    t.string   "string"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
